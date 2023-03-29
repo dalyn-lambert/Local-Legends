951f40991654a06de15d6bfa50ce6b914734e17d
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-03-29 16:10:15 -0500

    set up tailwind, added custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      purple: '#655AA9',
+      red: '#C73329',
+      'dk-gray': '#51515A',
+      'md-gray': '#999CA1',
+      'lt-gray': '#D6D6D6',
+      yellow: '#FFCB32',
+      'dk-yellow': '#95741E',
+      'dk-blue': '#14203C',
+      black: '#202020',
+      white: 'FFFFFF',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
