28adfb3f4717d330e7f015cac446330f51a39f86
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-04-11 18:35:25 -0500

    changed file name in CharacterCard

--- components/CharacterCard.tsx
+++ components/CharacterCard.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const CharacterCard = () => {
   return (
     <div className='bg-dk-gray w-[150px] h-[250px] mb-4 border-4 border-lt-gray rounded'>
-      <Image src='/assets/portraits/Fox_Palette_(SSBM).png' alt='Fox' width={150} height={150} />
+      <Image src='/assets/portraits/Fox.png' alt='Fox' width={150} height={150} />
     </div>
   );
 };
