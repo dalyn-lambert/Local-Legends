488b6c2d4ff42f247a99ccd05ea20a879fa4e9c1
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-04-10 19:06:55 -0500

    added character portraits

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,3 +1,10 @@
+import Image from 'next/image';
+
 export default function Events() {
-  return <div>Events</div>;
+  return (
+    <div>
+      <span>Events</span>
+      <Image src='/assets/icons/BowserHeadSSBM.png' alt='Bowser' width={15} height={15} />
+    </div>
+  );
 }
