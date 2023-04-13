25150f1b0810400a9e2b8e8e22a619976e81ba0b
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-04-13 16:03:16 -0500

    started styling character card

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default async function Matches() {
   return (
     <div className='flex flex-cols-2 w-3/4 gap-8 justify-between px-7'>
       <div>
-        <CharacterCard />
-        <CharacterCard />
+        <CharacterCard flag='Player 1' character='Bowser' />
+        <CharacterCard flag='Player 2' character='Captain Falcon' />
       </div>
       <div className='grow'>
         {matches.map((match) => (
--- components/CharacterCard.tsx
+++ components/CharacterCard.tsx
@@ -1,9 +1,29 @@
 import Image from 'next/image';
 
-const CharacterCard = () => {
+type CharacterCardProps = { flag: string; character: string };
+
+const CharacterCard = ({ flag, character }: CharacterCardProps) => {
   return (
-    <div className='bg-dk-gray w-[150px] h-[250px] mb-4 border-4 border-lt-gray rounded'>
-      <Image src='/assets/portraits/Fox.png' alt='Fox' width={150} height={150} />
+    <div className='hover:drop-shadow-2xl shadow-dk-purple bg-dk-gray w-[175px] h-[275px] mb-4 border-4 border-md-gray rounded flex flex-col gap-2 items-center'>
+      <div className='absolute w-[75px] h-[25px] bg-yellow drop-shadow-lg rounded shadow-dk-yellow text-xs text-black text-center flex flex-col justify-center -translate-y-2 -translate-x-14'>
+        {flag}
+      </div>
+      <Image
+        src={`/assets/portraits/${character}.png`}
+        width={100}
+        height={100}
+        alt={`Portrait of
+      ${character}`}
+        className='pt-8 translate-x-[34px]'
+      />
+      <div className='flex flex-col items-center'>
+        <div className='bg-dk-blue w-[150px] h-[75px] border-2 border-md-gray rounded-sm text-white text-center flex flex-col justify-end pb-1'>
+          {character}
+        </div>
+        <div className='bg-black w-[125px] h-[50px] border-2 border-md-gray rounded-sm text-white text-center flex flex-col justify-center -translate-y-[90px]'>
+          Drop Down Here
+        </div>
+      </div>
     </div>
   );
 };
