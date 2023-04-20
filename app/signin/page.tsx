2bb81e1e632fef122cb0d1f283fabfe223b66324
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-04-20 15:42:58 -0500

    started code for signin - not working yet

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,14 +1,41 @@
-export default function SignIn() {
+'use client';
+import { signin } from '@/lib/api';
+import { User } from '@prisma/client';
+import { useRouter } from 'next/navigation';
+import { FormEvent, useCallback, useState } from 'react';
+
+const initial: Partial<User> = { email: '', password: '' };
+
+export default function SignInPage() {
+  const [formState, setFormState] = useState({ ...initial });
+  const [error, setError] = useState('');
+
+  const router = useRouter();
+  const handleSubmit = useCallback(
+    async (e: FormEvent) => {
+      e.preventDefault();
+
+      try {
+        await signin(formState);
+        router.replace('/');
+      } catch (e) {
+        setError('Could not sign in');
+      } finally {
+        setFormState({ ...initial });
+      }
+    },
+    [formState, router]
+  );
   return (
     <>
-      <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-dk-purple rounded-md'>
+      <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 drop-shadow-lg bg-dk-purple rounded-md'>
         <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
           <h2 className='text-center text-2xl font-bold leading-9 tracking-tight text-white'>
             Sign in to edit content
           </h2>
         </div>
         <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-          <form className='space-y-6' action='#' method='POST'>
+          <form onSubmit={handleSubmit} className='space-y-6' action='#' method='POST'>
             <div>
               <label htmlFor='email' className='block text-sm font-medium leading-6 text-white'>
                 Email
@@ -21,7 +48,9 @@ export default function SignIn() {
                   autoComplete='email'
                   placeholder='fox@corneria.gov'
                   required
-                  className='block w-full rounded-md border-0 py-1.5 pl-1 bg-lt-gray text-white shadow-sm ring-1 ring-inset ring-dk-gray placeholder:text-dk-gray focus:ring-2 focus:ring-inset focus:ring-dk-purple sm:text-sm sm:leading-6'
+                  value={formState.email}
+                  onChange={(e) => setFormState((s) => ({ ...s, email: e.target.value }))}
+                  className='block w-full rounded-md border-0 py-1.5 pl-1 bg-lt-gray text-black shadow-sm ring-1 ring-inset ring-dk-gray placeholder:text-dk-gray focus:ring-2 focus:ring-inset focus:ring-dk-purple sm:text-sm sm:leading-6'
                 />
               </div>
             </div>
@@ -39,7 +68,9 @@ export default function SignIn() {
                   autoComplete='current-password'
                   placeholder='*****'
                   required
-                  className='block w-full rounded-md border-0 pl-1 py-1.5 bg-lt-gray text-white shadow-sm ring-1 ring-inset ring-dk-gray placeholder:text-dk-gray focus:ring-2 focus:ring-inset focus:ring-dk-purple sm:text-sm sm:leading-6'
+                  value={formState.password}
+                  onChange={(e) => setFormState((s) => ({ ...s, password: e.target.value }))}
+                  className='block w-full rounded-md border-0 pl-1 py-1.5 bg-lt-gray text-black shadow-sm ring-1 ring-inset ring-dk-gray placeholder:text-dk-gray focus:ring-2 focus:ring-inset focus:ring-dk-purple sm:text-sm sm:leading-6'
                 />
               </div>
             </div>
--- lib/api.ts
+++ lib/api.ts
@@ -1,15 +1,24 @@
-const fetcher = async ({ url, method, body, json = true }) => {
+import { User } from '@prisma/client';
+
+type FetcherProps = {
+  url: string;
+  method: string;
+  body: Partial<User>;
+  json?: boolean;
+};
+
+const fetcher = async ({ url, method, body, json = true }: FetcherProps) => {
   const res = await fetch(url, {
     method,
     body: body && JSON.stringify(body),
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     },
   });
 
   if (!res.ok) {
-    throw new Error("API Error");
+    throw new Error('API Error');
   }
 
   if (json) {
@@ -18,10 +27,11 @@ const fetcher = async ({ url, method, body, json = true }) => {
   }
 };
 
-export const signin = async (user) => {
+export const signin = async (user: User) => {
+  console.log('sign in function here');
   return fetcher({
-    url: "/api/signin",
-    method: "POST",
+    url: 'api/signin',
+    method: 'POST',
     body: user,
     json: false,
   });
