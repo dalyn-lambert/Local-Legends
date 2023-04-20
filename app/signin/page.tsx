74cd1932dc85c89deb4a5e0bdc4485a0517c106b
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-04-20 14:50:15 -0500

    changes margin of signin

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -3,7 +3,7 @@ export default function SignIn() {
     <>
       <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-dk-purple rounded-md'>
         <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-          <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white'>
+          <h2 className='text-center text-2xl font-bold leading-9 tracking-tight text-white'>
             Sign in to edit content
           </h2>
         </div>
